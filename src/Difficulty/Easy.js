4a676afbc1db4b9e983a6b726e7123d1cffce4e3
lidazhao1 <https://github.com/Spring-2021-CISC374/typescript-practice-lidazhao1>
2021-04-06 22:04:26 +0800

    lida commit it

--- src/Difficulty/Easy.js
+++ src/Difficulty/Easy.js
@@ -5,6 +5,25 @@ class Easy extends Phaser.Scene {
     }
   
     create() {
+      this.table = this.add.image(100, 150, "table").setOrigin(0).setScale(1.5)
+      this.apple = this.add.image(100, 150, "apple").setOrigin(0).setScale(1).setInteractive()
+      .on('pointerdown', () => this.appleInfo());
+      this.chicken = this.add.image(200, 150, "chicken").setOrigin(0).setScale(1).setInteractive()
+      .on('pointerdown', () => this.chickInfo());
+      this.popcorn = this.add.image(300, 150, "popcorn").setOrigin(0).setScale(1).setInteractive()
+      .on('pointerdown', () => this.popcornInfo());
+      this.cookie = this.add.image(400, 150, "cook").setOrigin(0).setScale(1).setInteractive()
+      .on('pointerdown', () => this.cookieInfo());
+      this.cupcake = this.add.image(500, 150, "cupcake").setOrigin(0).setScale(1).setInteractive()
+      .on('pointerdown', () => this.cupcakeInfo());
+      this.sushi = this.add.image(600, 150, "sushi").setOrigin(0).setScale(1).setInteractive()
+      .on('pointerdown', () => this.sushiInfo());
+      this.hamgurger = this.add.image(200, 250, "hamgurger").setOrigin(0).setScale(1).setInteractive()
+      .on('pointerdown', () => this.hamgurgerInfo());
+      this.incecream = this.add.image(300, 250, "icecream").setOrigin(0).setScale(1).setInteractive()
+      .on('pointerdown', () => this.incecreamInfo());
+      this.juice = this.add.image(400, 250, "juice").setOrigin(0).setScale(1).setInteractive()
+      .on('pointerdown', () => this.juiceInfo());
       const backButton = this.add.text(config.width - 200, config.height - 40, 'Back To Mode Menu', {
         font: "20px Arial",
         fill: 'black'
@@ -16,6 +35,38 @@ class Easy extends Phaser.Scene {
     updateScene() {
       this.scene.start("levelSetting");
     }
-    
+    appleInfo(){
+      alert("This is an apple about 150 grams, edible parts about 135 grams, contains 60 kcal of energy");
+    }
+    chickInfo(){
+      alert("This is a roast chicken. It weighs about 500 grams and contains 972 kcal of energy");
+    }
 
+    popcornInfo(){
+      alert("This is a bucket of popcorn, weighing about 300 grams, and contains 1000 kcal of energy");
+    }
+
+    cookieInfo(){
+      alert("This is a cookie that weighs about 6 grams and contains 26 kilocalories");
+    }
+
+    cupcakeInfo(){
+      alert("This is a cake that weighs about 80 grams and contains 278 kilocalories");
+    }
+
+    sushiInfo(){
+      alert("This is a sushi that weighs about 100 grams and contains 120 kilocalories");
+    }
+
+    hamgurgerInfo(){
+      alert("This is a hamberger that weighs about 120 grams and contains 354 kilocalories");
+    }
+
+    incecreamInfo(){
+      alert("This is a 66 gram ice cream with 137 kilocalories");
+    }
+
+    juiceInfo(){
+      alert("This is a glass of 249 grams of juice with 136 kilocalories");
+    }
   }
--- src/Difficulty/Hard.js
+++ src/Difficulty/Hard.js
@@ -5,6 +5,25 @@ class Hard extends Phaser.Scene {
     }
   
     create() {
+      this.table = this.add.image(100, 150, "table").setOrigin(0).setScale(1.5)
+      this.apple = this.add.image(100, 150, "apple").setOrigin(0).setScale(1).setInteractive()
+      .on('pointerdown', () => this.Info());
+      this.chicken = this.add.image(200, 150, "chicken").setOrigin(0).setScale(1).setInteractive()
+      .on('pointerdown', () => this.Info());
+      this.popcorn = this.add.image(300, 150, "popcorn").setOrigin(0).setScale(1).setInteractive()
+      .on('pointerdown', () => this.Info());
+      this.cookie = this.add.image(400, 150, "cook").setOrigin(0).setScale(1).setInteractive()
+      .on('pointerdown', () => this.Info());
+      this.cupcake = this.add.image(500, 150, "cupcake").setOrigin(0).setScale(1).setInteractive()
+      .on('pointerdown', () => this.Info());
+      this.sushi = this.add.image(600, 150, "sushi").setOrigin(0).setScale(1).setInteractive()
+      .on('pointerdown', () => this.Info());
+      this.hamgurger = this.add.image(200, 250, "hamgurger").setOrigin(0).setScale(1).setInteractive()
+      .on('pointerdown', () => this.Info());
+      this.incecream = this.add.image(300, 250, "icecream").setOrigin(0).setScale(1).setInteractive()
+      .on('pointerdown', () => this.Info());
+      this.juice = this.add.image(400, 250, "juice").setOrigin(0).setScale(1).setInteractive()
+      .on('pointerdown', () => this.Info());
       const backButton = this.add.text(config.width - 200, config.height - 40, 'Back To Mode Menu', {
         font: "20px Arial",
         fill: 'black'
@@ -16,6 +35,8 @@ class Hard extends Phaser.Scene {
     updateScene() {
       this.scene.start("levelSetting");
     }
+    Info(){
+      alert("This is the Hard mode, no tips here!");
+    }
     
-
   }
